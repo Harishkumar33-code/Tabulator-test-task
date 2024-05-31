79aa672c9ecd4be6a18d4a095b5cd400f04de6c6
Harishkumar T <harishkumar.thirupathi@softsuave.com>
2024-05-31 18:07:14 +0530

    stable app

--- src/EditableTable.js
+++ src/EditableTable.js
@@ -1,30 +1,14 @@
 import React, { useState, useEffect, useRef } from "react";
-import { ReactTabulator } from "react-tabulator";
+import { ReactTabulator } from 'react-tabulator';
 import "tabulator-tables/dist/css/tabulator.min.css";
 import "react-tabulator/lib/styles.css";
 import "./EditableTable.css";
 
 const EditableTable = () => {
-  const [data, setData] = useState([
-    { id: 1, name: "John", age: 28, gender: "Male", rating: 4.5, editable: false },
-    { id: 2, name: "Jane", age: 32, gender: "Female", rating: 4.0, editable: false},
-    { id: 3, name: "Harish", age: 26, gender: "Male", rating: 4.5, editable: false},
-    { id: 4, name: "Kani", age: 29, gender: "Male", rating: 4.0, editable: false},
-    { id: 5, name: "Madhu", age: 35, gender: "Male", rating: 4.5, editable: false },
-    { id: 6, name: "Vignesh", age: 25, gender: "Male", rating: 4.0, editable: false },
-    { id: 7, name: "Mari", age: 28, gender: "Male", rating: 4.5, editable: false },
-    { id: 8, name: "saatty", age: 25, gender: "Male", rating: 4.0, editable: false },
-    { id: 9, name: "Su", age: 26, gender: "Male", rating: 4.5,editable: false },
-    { id: 10, name: "Ashwin", age: 26, gender: "Male", rating: 4.6, editable: false },
-  ]);
+  const [data, setData] = useState([]);
   const [selectedRows, setSelectedRows] = useState([]);
   const [editMode, setEditMode] = useState(false);
   const tableRef = useRef(null);
-
-  const editCheck = function(cell){
-    var data = cell.getRow().getData();
-    return data.editable;
-  };
 
   const columns = [
     {
@@ -36,15 +20,14 @@ const EditableTable = () => {
         cell.getRow().toggleSelect();
       },
     },
-    { title: "Name", field: "name", headerFilter: "input", editor: "input", editable: editCheck},
-    { title: "Age", field: "age", headerFilter: "input", editor: "input",editable: editCheck},
+    { title: "Name", field: "name", headerFilter: "input", editor: "input" },
+    { title: "Age", field: "age", headerFilter: "input", editor: "input" },
     {
       title: "Gender",
       field: "gender",
       headerFilter: "select",
       headerFilterParams: { values: ["", "Male", "Female"] },
-      editor: "input",
-      editable: editCheck,
+      editor: "select",
       editorParams: { values: ["Male", "Female"] },
     },
     {
@@ -52,50 +35,38 @@ const EditableTable = () => {
       field: "rating",
       headerFilter: "input",
       editor: "input",
-      editable: editCheck,
-    },];
+    },
+  ];
 
-
-  
-
-  // useEffect(() => {
-  //   const fetchData = async () => {
-  //     setTimeout(() => {
-  //       const result = [
-  //         { id: 1, name: "John", age: 28, gender: "Male", rating: 4.5, editable: false },
-  //         { id: 2, name: "Jane", age: 32, gender: "Female", rating: 4.0, editable: false},
-  //         { id: 3, name: "Harish", age: 26, gender: "Male", rating: 4.5, editable: false},
-  //         { id: 4, name: "Kani", age: 29, gender: "Male", rating: 4.0, editable: false},
-  //         { id: 5, name: "Madhu", age: 35, gender: "Male", rating: 4.5, editable: false },
-  //         { id: 6, name: "Vignesh", age: 25, gender: "Male", rating: 4.0, editable: false },
-  //         { id: 7, name: "Mari", age: 28, gender: "Male", rating: 4.5, editable: false },
-  //         { id: 8, name: "saatty", age: 25, gender: "Male", rating: 4.0, editable: false },
-  //         { id: 9, name: "Su", age: 26, gender: "Male", rating: 4.5,editable: false },
-  //         { id: 10, name: "Ashwin", age: 26, gender: "Male", rating: 4.6, editable: false },
-  //       ];
-  //       setData(result);
-  //     }, 100);
-  //   };
-  //   fetchData();
-  // }, []);
+  useEffect(() => {
+    const fetchData = async () => {
+      setTimeout(() => {
+        const result = [
+          { id: 1, name: "John", age: 28, gender: "Male", rating: 4.5 },
+          { id: 2, name: "Jane", age: 32, gender: "Female", rating: 4.0 },
+          { id: 3, name: "Harish", age: 26, gender: "Male", rating: 4.5 },
+          { id: 4, name: "Kani", age: 29, gender: "Male", rating: 4.0 },
+          { id: 5, name: "Madhu", age: 35, gender: "Male", rating: 4.5 },
+          { id: 6, name: "Vignesh", age: 25, gender: "Male", rating: 4.0 },
+          { id: 7, name: "Mari", age: 28, gender: "Male", rating: 4.5 },
+          { id: 8, name: "saatty", age: 25, gender: "Male", rating: 4.0 },
+          { id: 9, name: "Su", age: 26, gender: "Male", rating: 4.5 },
+          { id: 10, name: "Ashwin", age: 26, gender: "Male", rating: 4.6 },
+        ];
+        setData(result);
+      }, 100);
+    };
+    fetchData();
+  }, []);
 
   const handleRowSelection = (selectedData) => {
     setEditMode(false);
-    console.log(selectedData);
-    setSelectedRows([...selectedRows,selectedData.id]);
+    setSelectedRows(selectedData);
   };
 
   const handleEdit = () => {
-    setData((prevData) => {
-      return prevData.map((row) => {
-        if (selectedRows.includes(row.id)) {
-          return { ...row, editable: true };
-        }
-        return row;
-      });
-    })
+    setEditMode(true);
   };
-
 
   const handleSave = () => {
     setEditMode(false);
@@ -124,7 +95,6 @@ const EditableTable = () => {
     },
   };
 
-
   return (
     <div>
       <div>TABULATOR</div>
@@ -141,9 +111,7 @@ const EditableTable = () => {
           </button>
         </div>
       )}
-      {
-        selectedRows
-      }
+
       <ReactTabulator
         ref={tableRef}
         columns={columns}
@@ -155,7 +123,7 @@ const EditableTable = () => {
           headerFilterPlaceholder: "Filter...",
           headerSortTristate: true,
           movableRows: true,
-          pagination: "local",
+          pagination:"local",
           paginationSize: 3,
         }}
       />
